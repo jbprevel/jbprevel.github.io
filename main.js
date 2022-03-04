9ee52e14359b7f40b1e0d9fc13e85907b88c1f2d
Prevel Jean-Baptiste <jb.prevel50@gmail.com>
2022-03-04 15:00:49 +0100

    mis a jour avec le h1 animé

--- main.js
+++ main.js
@@ -22,3 +22,29 @@ navLinks.forEach((link) => {
   });
 });
 
+// animation du texte dans le header
+
+const txtAnim = document.querySelector("h1"); //on cible notre element h1
+
+new Typewriter(txtAnim, {
+  //on appelle la fonction que l'on a importé dans le script en html
+  //loop: true, a mettre a mettre pour boucler a l'infini
+  deleteSpeed: 25, // modifie la vitesse d'effacement
+})
+  .changeDelay(50) //on peut changer la vitesse d'ecriture
+  .typeString("Moi c' est JB") // on ecrit ce que l'on souhaite
+  .pauseFor(1000) // on fait une pause entre deux "phrases"
+  .typeString(", Passionné de Développement WEB") //on ajoute une balise span pour pouvoir y injecter du style
+  .pauseFor(1000)
+  .deleteChars(17) //on supprime les lettres voulues en comptant les espaces
+  .typeString('<span style="color: #8a307f;"> Design</span>')
+  .pauseFor(1000)
+  .deleteChars(6)
+  .typeString('<span style="color: #12a4d9;"> CSS</span>')
+  .pauseFor(1000)
+  .deleteChars(3)
+  .typeString('<span style="color: yellow;"> JavaScript</span>')
+  .pauseFor(1000)
+  .deleteChars(11)
+  .typeString(" Développement WEB")
+  .start(); // on lance la fonction;
